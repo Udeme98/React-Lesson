461ec222994f8f2e7ccb280e9d427d17f27c1d92
Udeme98 <Darealudeme1@gmail.com>
2024-02-26 05:53:04 +0100

    refactoredbooklist.jsx code

--- src/Lessons/BookList.jsx
+++ src/Lessons/BookList.jsx
@@ -1,25 +1,10 @@
 const BookList = () => {
-  const Image = () => (
-    <img src="../../public/images/amazon-best.jpg" alt="novel" />
-  );
-  const Title = () => {
-    const inlineHeadingStyles = {
-      color: "#223",
-    };
-    return <h2 style={inlineHeadingStyles}>The Women</h2>;
-  };
-  const Author = () => (
-    <h4 style={{ color: "#617d98", fontSize: "0.8rem", marginTop: "0.5rem" }}>
-      Kristin Hannah
-    </h4>
-  );
-
   const Book = () => {
     return (
       <article className="book">
-        <Image />
-        <Title />
-        <Author />
+        <img src="../../public/images/amazon-best.jpg" alt="novel" />
+        <h2>The Women</h2>
+        <h4>Kristin Hannah</h4>
       </article>
     );
   };
