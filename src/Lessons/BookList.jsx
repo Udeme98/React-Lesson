6588040f68672563b12505d4e485106df02b6a1e
Udeme98 <Darealudeme1@gmail.com>
2024-02-26 05:42:28 +0100

    using inline jsx styles object

--- src/Lessons/BookList.jsx
+++ src/Lessons/BookList.jsx
@@ -2,7 +2,12 @@ const BookList = () => {
   const Image = () => (
     <img src="../../public/images/amazon-best.jpg" alt="novel" />
   );
-  const Title = () => <h2>The Women</h2>;
+  const Title = () => {
+    const inlineHeadingStyles = {
+      color: "#223",
+    };
+    return <h2 style={inlineHeadingStyles}>The Women</h2>;
+  };
   const Author = () => (
     <h4 style={{ color: "#617d98", fontSize: "0.8rem", marginTop: "0.5rem" }}>
       Kristin Hannah
