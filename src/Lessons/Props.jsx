1b584a5704c0d76ed576d03d6b6dd834b392301a
Udeme98 <Darealudeme1@gmail.com>
2024-02-27 05:20:42 +0100

    props - somewhat dynamic

--- src/Lessons/Props.jsx
+++ src/Lessons/Props.jsx
@@ -1,6 +1,14 @@
-const title = "The Woman";
-const author = "Kristin Hannah";
-const img = "images/amazon-best.jpg";
+const firstBook = {
+  title: "The Woman",
+  author: "Kristin Hannah",
+  img: "images/amazon-best.jpg",
+};
+
+const secondBook = {
+  title: "Atomic Habit",
+  author: "James Clear",
+  img: "images/atomic-habits.png",
+};
 
 const Book = (props) => {
   return (
@@ -15,8 +23,16 @@ const Book = (props) => {
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book title={title} author={author} img={img} />
-      <Book title={title} author={author} img={img} />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
     </section>
   );
 };
