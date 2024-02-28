082b14e282f9bdef601547fd1eb66fb0d60a42c1
Udeme98 <Darealudeme1@gmail.com>
2024-02-28 05:48:18 +0100

    updated props.jsx

--- src/Lessons/Props.jsx
+++ src/Lessons/Props.jsx
@@ -1,15 +1,24 @@
+// Topics
+// * Props in React
+// * Props - somewhat dynamic
+// * Props - multiple approaches
+// * Children props
+// * Props - Simple/Proper list
+// * Key prop
+// * Passing entire object as props
+
 const books = [
   {
+    id: 1,
     title: "The Woman",
     author: "Kristin Hannah",
     img: "images/amazon-best.jpg",
-    id: 1,
   },
   {
+    id: 2,
     title: "Atomic Habit",
     author: "James Clear",
     img: "images/atomic-habits.png",
-    id: 2,
   },
 ];
 
