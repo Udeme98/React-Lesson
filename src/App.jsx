5f3c679f3dfd7e88bf3bcab8ea4aee1d4518bc84
Udeme98 <Darealudeme1@gmail.com>
2024-03-03 20:34:11 +0100

    updated file/folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Vite from "./Lessons/Advanced-React/Vite";
 
 const App = () => {
   return (
-    <>
-      <Vite />
-    </>
+    <div className="container">
+      <h2>Advanced React</h2>
+    </div>
   );
 };
 
