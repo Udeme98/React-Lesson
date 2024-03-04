1acc26fe100e5bce7acf569e7addb5c63203dcf4
Udeme98 <Darealudeme1@gmail.com>
2024-03-04 22:50:42 +0100

    updated useStateArray

--- src/Lessons/Advanced-React/useState/useStateArray.jsx
+++ src/Lessons/Advanced-React/useState/useStateArray.jsx
@@ -6,7 +6,40 @@
 // i. one that removes single item from the list
 // ii. one that clears entire list
 
+import { data } from "../../../data";
+import { useState } from "react";
+
 const UseStateArray = () => {
-  return <div>useStateArray</div>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    const rem = people.filter((person) => person.id !== id);
+    setPeople(rem);
+  };
+  const clearAll = () => setPeople([]);
+
+  const showPeople = people.map((person) => {
+    const { name, id } = person;
+    return (
+      <>
+        <h4 key={id}>{name}</h4>
+        <button onClick={() => removeItem(id)}>Remove Item</button>
+      </>
+    );
+  });
+
+  return (
+    <div>
+      <div>{showPeople}</div>
+      <button
+        type="button"
+        onClick={clearAll}
+        style={{ marginTop: "2rem" }}
+        className="btn"
+      >
+        Clear All
+      </button>
+    </div>
+  );
 };
 export default UseStateArray;
