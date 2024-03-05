c1f88f6b0360bacd99171ad4164f77cfb86fa571
Udeme98 <Darealudeme1@gmail.com>
2024-03-05 11:01:38 +0100

    updated useStateGotcha

--- src/Lessons/Advanced-React/useState/useStateGotcha.jsx
+++ src/Lessons/Advanced-React/useState/useStateGotcha.jsx
@@ -19,6 +19,20 @@ const UseStateGotcha = () => {
       >
         Increase
       </button>
+      <br />
+      <br />
+
+      <h2>usestate function approach</h2>
+      <p>
+        If you want to update the state immediately and synchronousy, you can
+        pass a function to setState that receives the previous state as an
+        argument and returns the new state.
+      </p>
+      <pre>//setState(prevState=(...prevState, value:newValue))</pre>
+      <p>
+        This can be useful if you need to update the state based on the previous
+        state, or if you need to update the state synchronously.
+      </p>
     </>
   );
 };
