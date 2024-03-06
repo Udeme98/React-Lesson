90b8fc8526c6a7369af5d51ac0899467601d4173
Udeme98 <Darealudeme1@gmail.com>
2024-03-06 05:59:14 +0100

    updated useEffectBasics

--- src/Lessons/Advanced-React/useEffect/useEffectBasics.jsx
+++ src/Lessons/Advanced-React/useEffect/useEffectBasics.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
@@ -9,6 +9,10 @@ const UseEffectBasics = () => {
   };
   //sayHello();
 
+  useEffect(() => {
+    sayHello();
+  }, []);
+
   return (
     <div>
       <h1>useEffect Basics</h1>
@@ -16,7 +20,7 @@ const UseEffectBasics = () => {
         useEffect is a hook in React that allows you to perform side effects in
         function component (basically any work outside of the component).
       </p>
-      Some examples of side effects are:
+      <h4>Some examples of side effects are:</h4>
       <ul>
         <li>a. subscriptions</li>
         <li>b. fetching data</li>
@@ -24,7 +28,20 @@ const UseEffectBasics = () => {
         <li>d. event listener</li>
         <li>e. timers, etc</li>
       </ul>
-      <h2>Value: {value}</h2>
+
+      <h4>Important Info:</h4>
+      <ul>
+        <li>i. useEffect hook accepts two arguments (second optional)</li>
+        <li>ii. first argument (callback function)</li>
+        <li>iii. second argument (dependency array)</li>
+        <li>iv. by default, runs on each render (initial and re-render)</li>
+        <li>v. callbacks can't return promise (so can't make it async)</li>
+        <li>
+          vi. if dependency array is empty ([]), runs only on initial render
+        </li>
+      </ul>
+
+      <h3>Value: {value}</h3>
       <button
         className="btn"
         style={{ marginTop: "2rem" }}
