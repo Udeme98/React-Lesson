499ced72ed0a0085e96e660e395f7f20dd7d5158
Udeme98 <Darealudeme1@gmail.com>
2024-03-07 02:16:59 +0100

    conditional rendering - multiple returns fetchData complete

--- src/Lessons/Advanced-React/conditionalRendering/MultipleReturnsFetchData.jsx
+++ src/Lessons/Advanced-React/conditionalRendering/MultipleReturnsFetchData.jsx
@@ -7,19 +7,53 @@
 import { useEffect, useState } from "react";
 
 const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null);
 
   const url = "https://api.github.com/users/QuincyLarson";
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
     };
     fetchUser();
   }, []);
 
-  return <div>Multiple Returns Fetch Data</div>;
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>There was an error...</h3>;
+  }
+
+  return (
+    <div>
+      {/* <h2>Multiple Returns Fetch Data</h2>
+      <h3>Data Fetching</h3>
+      <h4>Usually 3 options:</h4>
+      <ul>
+        <li>i. loading - waiting for data to arrive (display loading state)</li>
+        <li>ii. error - there was an error (display error message)</li>
+        <li>iii. success - received data (display data)</li>
+      </ul> */}
+      <img
+        src={user.avatar_url}
+        alt={user.name}
+        style={{ borderRadius: "25px", width: "150px" }}
+      />
+      <h2>{user.name}</h2>
+      <h4>Works at {user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
