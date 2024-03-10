08b249e2e22697a7bb2557dd9dd5647016246175
Udeme98 <Darealudeme1@gmail.com>
2024-03-10 18:49:53 +0100

    forms - other inputs: select element

--- src/Lessons/Advanced-React/6-forms/OtherInputs.jsx
+++ src/Lessons/Advanced-React/6-forms/OtherInputs.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
-const framework = ["react", "vue", "angular", "svetle"];
+const frameworks = ["react", "vue", "angular", "svetle"];
 
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
 
   const handleChange = (e) => {
     console.log(e.target.checked);
     setShipping(e.target.checked);
   };
+
+  const handleFramework = (e) => {
+    console.log(e.target.value);
+    setFramework(e.target.value);
+  };
+
   return (
     <div>
       <form className="form">
@@ -28,6 +35,20 @@ const OtherInputs = () => {
           <label htmlFor="framework" className="form-label">
             framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            // value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((framework) => {
+              return (
+                <option value={framework} key={framework}>
+                  {framework}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
