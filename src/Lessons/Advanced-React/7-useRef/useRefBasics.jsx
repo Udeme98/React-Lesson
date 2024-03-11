1c377dc1c6605c0bc386359df0e212bd972e58ad
Udeme98 <Darealudeme1@gmail.com>
2024-03-11 19:45:15 +0100

    updated useRef basics

--- src/Lessons/Advanced-React/7-useRef/useRefBasics.jsx
+++ src/Lessons/Advanced-React/7-useRef/useRefBasics.jsx
@@ -1,4 +1,13 @@
 const UseRefBasics = () => {
-  return <div>useRef Basics</div>;
+  return (
+    <div>
+      <h2>useRef Hook</h2>
+      <ul style={{ textAlign: "left" }}>
+        <li>1. Like useState, it preserves the value between the renders.</li>
+        <li>2. Updating useRef does not trigger re-render.</li>
+        <li>3. Targets DOM nodes/elements.</li>
+      </ul>
+    </div>
+  );
 };
 export default UseRefBasics;
