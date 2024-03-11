8b38a533b774d664457f1c619226448a71f5311a
Udeme98 <Darealudeme1@gmail.com>
2024-03-11 21:36:03 +0100

    useRef hook:initial render

--- src/Lessons/Advanced-React/7-useRef/useRefBasics.jsx
+++ src/Lessons/Advanced-React/7-useRef/useRefBasics.jsx
@@ -3,10 +3,19 @@ import { useEffect, useRef, useState } from "react";
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    console.log(refContainer);
+    refContainer.current.focus();
   });
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log("re-rendered");
+  }, [value]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
