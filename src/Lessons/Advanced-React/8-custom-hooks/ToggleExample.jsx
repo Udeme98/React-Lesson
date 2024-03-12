754e623a9efee6a020e0119ab369abf8014f974c
Udeme98 <Darealudeme1@gmail.com>
2024-03-12 14:59:47 +0100

    updated custom hook

--- src/Lessons/Advanced-React/8-custom-hooks/ToggleExample.jsx
+++ src/Lessons/Advanced-React/8-custom-hooks/ToggleExample.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const ToggleExample = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
       <h2>Custom Hooks</h2>
+      <ul style={{ textAlign: "left" }}>
+        <li>1. Same rules as regular hooks.</li>
+        <li>2. Simplify component(less code).</li>
+        <li>3. Re-use functionality.</li>
+      </ul>
+      <br />
+
+      <h4>Toggle custom hook</h4>
+      <button onClick={() => setShow((prev) => !prev)} className="btn">
+        Toggle
+      </button>
+      {show && <h4>Some stuff</h4>}
     </div>
   );
 };
