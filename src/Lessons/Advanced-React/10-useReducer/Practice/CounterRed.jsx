7b4ae7f157516ae394ab359189d8425c7b282dcc
Udeme98 <Darealudeme1@gmail.com>
2024-03-26 16:00:48 +0100

    practicing usereducer

--- src/Lessons/Advanced-React/10-useReducer/Practice/CounterRed.jsx
+++ src/Lessons/Advanced-React/10-useReducer/Practice/CounterRed.jsx
@@ -1,4 +1,22 @@
+import { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increase":
+      state + 1;
+    case "decrease":
+      state - 1;
+    case "reset":
+      initialState;
+    default:
+      state;
+  }
+};
+
 const CounterRed = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <button>Increase</button>
