0090b0267e5ecd1b61f3d08516d830feca451e31
Udeme98 <Darealudeme1@gmail.com>
2024-03-27 10:57:10 +0100

    practicing usereducer pt2 - state as an object

--- src/Lessons/Advanced-React/10-useReducer/Practice/CounterRed2.jsx
+++ src/Lessons/Advanced-React/10-useReducer/Practice/CounterRed2.jsx
@@ -2,16 +2,23 @@ import { useReducer } from "react";
 
 const initialState = {
   firstCounter: 0,
+  secondCounter: 5,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "increase":
-      return { firstCounter: state.firstCounter + action.value };
+      return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrease":
-      return { firstCounter: state.firstCounter - action.value };
+      return { ...state, firstCounter: state.firstCounter - action.value };
     case "increase5":
-      return { firstCounter: state.firstCounter + action.value };
+      return { ...state, firstCounter: state.firstCounter + action.value };
+    case "increase-2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
+    case "decrease-2":
+      return { ...state, secondCounter: state.secondCounter - action.value };
+    case "increase5-2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
     case "reset":
       return initialState;
     default:
@@ -24,6 +31,7 @@ const CounterRed2 = () => {
 
   return (
     <div>
+      <h4>First Counter</h4>
       <h5>count - {count.firstCounter}</h5>
       <button
         className="btn"
@@ -43,6 +51,40 @@ const CounterRed2 = () => {
         className="btn"
         style={{ margin: "2px" }}
         onClick={() => dispatch({ type: "increase5", value: 5 })}
+      >
+        Increase 5
+      </button>
+      <button
+        className="btn"
+        style={{ margin: "2px" }}
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Reset
+      </button>
+
+      <br />
+      <br />
+
+      <h4>Second Counter</h4>
+      <h5>count - {count.secondCounter}</h5>
+      <button
+        className="btn"
+        style={{ margin: "2px" }}
+        onClick={() => dispatch({ type: "increase-2", value: 1 })}
+      >
+        Increase
+      </button>
+      <button
+        className="btn"
+        style={{ margin: "2px" }}
+        onClick={() => dispatch({ type: "decrease-2", value: 1 })}
+      >
+        Decrease
+      </button>
+      <button
+        className="btn"
+        style={{ margin: "2px" }}
+        onClick={() => dispatch({ type: "increase5-2", value: 5 })}
       >
         Increase 5
       </button>
