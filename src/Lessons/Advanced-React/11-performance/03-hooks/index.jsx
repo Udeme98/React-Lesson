6570a3bdd55116dc1d12079b461b73fd15bb11f8
Udeme98 <Darealudeme1@gmail.com>
2024-03-31 20:50:20 +0100

    useCallback hook

--- src/Lessons/Advanced-React/11-performance/03-hooks/index.jsx
+++ src/Lessons/Advanced-React/11-performance/03-hooks/index.jsx
@@ -1,6 +1,11 @@
 // React.memo(Component) - returns memoized component
 
-import { useState } from "react";
+// useCallback hook - The useCallback hook in React allows you to memoize a function. It takes two arguments: The first is the function you want to memoize, and the second is an array of dependencies. The hook will return a memoized version of the function that only changes if one of the values in the dependency array changes.
+
+// By memoizing the function, you can avoid unnecessary re-renders and improve the performance of your React application.
+// The function will only be re-created if one of it's dependencies changes, otherwise the same instance of the function will be returned.
+
+import { useCallback, useState } from "react";
 import { data } from "../../../../data";
 import List from "./List";
 
@@ -8,10 +13,13 @@ const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
-  const removePerson = (id) => {
-    const newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
+  const removePerson = useCallback(
+    (id) => {
+      const newPeople = people.filter((person) => person.id !== id);
+      setPeople(newPeople);
+    },
+    [people]
+  );
 
   return (
     <section>
