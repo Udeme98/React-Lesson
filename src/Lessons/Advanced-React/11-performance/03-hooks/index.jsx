2cbb0a4c2ff795347094db49338dec8670fe2b94
Udeme98 <Darealudeme1@gmail.com>
2024-03-31 21:12:32 +0100

    useMemo hook

--- src/Lessons/Advanced-React/11-performance/03-hooks/index.jsx
+++ src/Lessons/Advanced-React/11-performance/03-hooks/index.jsx
@@ -5,14 +5,18 @@
 // By memoizing the function, you can avoid unnecessary re-renders and improve the performance of your React application.
 // The function will only be re-created if one of it's dependencies changes, otherwise the same instance of the function will be returned.
 
-import { useCallback, useState } from "react";
+// useMemo hook - The useCallback hook in React allows you to memoize a value. It takes two arguments: The first is the function that returns the value you want to memoize, and the second is an array of dependencies. The hook will return a memoized value only changes if one of the values in the dependency array changes.
+
+import { useCallback, useMemo, useState } from "react";
 import { data } from "../../../../data";
 import List from "./List";
+import slowFunction from "./slowFunction";
 
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
+  const value = useMemo(() => slowFunction(), []);
   const removePerson = useCallback(
     (id) => {
       const newPeople = people.filter((person) => person.id !== id);
@@ -20,6 +24,8 @@ const LowerState = () => {
     },
     [people]
   );
+
+  console.log(value);
 
   return (
     <section>
